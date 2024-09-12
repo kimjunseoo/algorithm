aec3c25792c0c66ad2f0ced2ecab27f64bd3b824
Kim Junseo <zz0905k@naver.com>
2024-09-12 15:28:29 +0900

    20240912 DP

---
+++ 문제집/1463.py
@@ -0,0 +1,16 @@
+X = int(input())
+
+dp = [0 for _ in range(10**6 + 1)]
+
+for i in range(2, X+1):
+
+    if i % 3 == 0 and i % 2 == 0:
+        dp[i] = min(dp[i-1] + 1, dp[i // 3] + 1, dp[i // 2] + 1)
+    elif i % 3 == 0:
+        dp[i] = min(dp[i-1] + 1, dp[i // 3] + 1)
+    elif i % 2 == 0:
+        dp[i] = min(dp[i-1] + 1, dp[i // 2] + 1)
+    else:
+        dp[i] = dp[i-1] + 1
+
+print(dp[X])
